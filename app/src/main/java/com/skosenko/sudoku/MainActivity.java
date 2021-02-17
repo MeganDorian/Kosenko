b14a90f12f503727fd72550ed6e50539c0b3476f
julie <mishakollins68@gmail.com>
2021-02-17 23:00:54 +0300

    Added basic game logic

--- app/src/main/java/com/skosenko/sudoku/MainActivity.java
+++ app/src/main/java/com/skosenko/sudoku/MainActivity.java
@@ -2,13 +2,33 @@ package com.skosenko.sudoku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "Sudoku";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View newButton = findViewById(R.id.new_button);
+        newButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        //start game
+        startGame(1);
+    }
+
+    private void startGame(int i) {
+        Log.d(TAG, "clicked on " + i);
+        Intent intent = new Intent(MainActivity.this, Game.class);
+        intent.putExtra(Game.KEY_DIFFICULTY, i);
+        startActivity(intent);
     }
 }
